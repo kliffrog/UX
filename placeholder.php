64e77325f9114061a0ca3f1ed1e8b9403c4e66bf
kliffrog <kliffrog@gmail.com>
2016-08-05 16:18:05 -0500

    uploag files

---
+++ placeholder.php
@@ -0,0 +1,50 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<title>Pacific Trails Resort</title>
+		<link rel="stylesheet" href="styles.css" type="text/css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<h1> Thank You!</h1>
+			<div id="content">
+				<h3>We will be contacting you soon!</h3>
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<form action="#">
+					<input type="button" value = "Back" onclick="javascript:history.go(-1)" />
+				</form>
+			</div>
+		</div>
+	</body>
+=======
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<title>Pacific Trails Resort</title>
+		<link rel="stylesheet" href="styles.css" type="text/css" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<h1> Thank You!</h1>
+			<div id="content">
+				<h3>We will be contacting you soon!</h3>
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<br />
+				<form action="#">
+					<input type="button" value = "Back" onclick="javascript:history.go(-1)" />
+				</form>
+			</div>
+		</div>
+	</body>
+>>>>>>> origin/master
+</html>
